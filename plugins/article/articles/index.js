0aec82bd2c0e071d7455a61205e716108a8ece8f
Tong ZHAN <zhantong1994@163.com>
2019-04-10 21:04:05 +0800

    同步API修改

--- plugins/article/articles/index.js
+++ plugins/article/articles/index.js
@@ -1,96 +1,85 @@
 import React from "react";
-import {connect} from 'react-redux'
-import {List, Icon} from 'antd';
-import {requestArticleList} from "./store";
-import {buildArticleUrl} from "../article";
-import Link from 'next/link'
-import Moment from 'react-moment';
+import { connect } from "react-redux";
+import { List, Icon } from "antd";
+import { requestArticleList } from "./store";
+import { buildArticleUrl } from "../article";
+import Link from "next/link";
+import Moment from "react-moment";
 
 class Articles extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    static async getInitialProps({reduxStore, query}) {
-        await reduxStore.dispatch(requestArticleList(query));
-        return {};
-    }
+  static async getInitialProps({ reduxStore, query }) {
+    await reduxStore.dispatch(requestArticleList(query));
+    return {};
+  }
 
-    render() {
-        const {articles = []} = this.props;
-        return (
-            <List
-                itemLayout="vertical"
-                dataSource={articles}
-                renderItem={item => {
-                    const articleUrl = buildArticleUrl(item.number);
-                    return (
-                        <List.Item
-                            actions={[
-                                <IconText type='calendar'>
-                                    <Moment format='YYYY-MM-DD'>
-                                        {item.timestamp}
-                                    </Moment>
-                                </IconText>,
-                                ...item.meta.map(meta => (
-                                    <Meta meta={meta}/>
-
-
-                                ))
-                            ]}
-                        >
-                            <List.Item.Meta
-                                title={<Link as={articleUrl.as} href={articleUrl.href}><a>{item.title}</a></Link>}
-                            />
-                            {item.bodyAbstract}
-                        </List.Item>
-                    )
-                }}
+  render() {
+    const { articles = [] } = this.props;
+    return (
+      <List
+        itemLayout="vertical"
+        dataSource={articles}
+        renderItem={item => {
+          const articleUrl = buildArticleUrl(item.number);
+          return (
+            <List.Item
+              actions={[
+                <IconText type="calendar">
+                  <Moment format="YYYY-MM-DD">{item.timestamp}</Moment>
+                </IconText>,
+                ...Object.keys(item.plugin).map(id => (
+                  <Meta id={id} meta={item.plugin[id]} />
+                ))
+              ]}
             >
-            </List>
-        )
-    }
+              <List.Item.Meta
+                title={
+                  <Link as={articleUrl.as} href={articleUrl.href}>
+                    <a>{item.title}</a>
+                  </Link>
+                }
+              />
+              {item.bodyAbstract}
+            </List.Item>
+          );
+        }}
+      />
+    );
+  }
 }
 
-const IconText = (props) => (
-    <span>
-    <Icon type={props.type} style={{marginRight: 8}}/>
-        {props.children}
+const IconText = props => (
+  <span>
+    <Icon type={props.type} style={{ marginRight: 8 }} />
+    {props.children}
   </span>
 );
 
-const Meta = ({meta}) => {
-    if (meta.slug === 'category') {
-        return (
-            <>
-                {meta.value.map(category =>
-                    <IconText type='folder-open'>
-                        {category.name}
-                    </IconText>
-                )}
-            </>
-        )
-    }
-    if (meta.slug === 'view_count') {
-        return (
-            <IconText type='heart'>
-                {meta.value}
-            </IconText>
-        )
-    }
-    if (meta.slug === 'comment') {
-        return (
-            <IconText type='message'>
-                {meta.value}
-            </IconText>
-        )
-    }
-    return null
+const Meta = ({ id, meta }) => {
+  if (id === "category") {
+    return (
+      <>
+        {meta.map(category => (
+          <IconText type="folder-open">{category.name}</IconText>
+        ))}
+      </>
+    );
+  }
+  if (id === "view_count") {
+    return <IconText type="heart">{meta.value}</IconText>;
+  }
+  if (id === "comment") {
+    return <IconText type="message">{meta.value}</IconText>;
+  }
+  return null;
 };
 
 function mapStateToProps(state) {
-    const {articles} = state.articles;
-    return {articles}
+  const { articles } = state.articles;
+  return { articles };
 }
 
-export default connect(mapStateToProps)(Articles)
+export default connect(mapStateToProps)(Articles);
