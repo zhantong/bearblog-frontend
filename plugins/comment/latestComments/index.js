f0b3e071d1659506b2cc7a1576e9161a9c72cb0d
Tong ZHAN <zhantong1994@163.com>
2019-04-20 16:14:54 +0800

    优化评论显示

--- plugins/comment/latestComments/index.js
+++ plugins/comment/latestComments/index.js
@@ -1,67 +1,80 @@
 import React from "react";
 import { requestLatestComments } from "./store";
 import { connect } from "react-redux";
-import { Collapse, List, Typography } from 'antd'
+import { Collapse, List, Typography } from "antd";
 import { buildArticleUrl } from "../../article/article";
-import Link from 'next/link'
-import ReactMarkdown from 'react-markdown'
+import Link from "next/link";
+import ReactMarkdown from "react-markdown";
 
 class LatestComments extends React.Component {
+  componentDidMount() {
+    this.props.requestLatestComments();
+  }
 
-    componentDidMount() {
-        this.props.requestLatestComments();
+  render() {
+    if (!this.props.latestComments) {
+      return null;
     }
 
-    render() {
-        if (!this.props.latestComments) {
-            return null;
-        }
-
-        return (
-            <Collapse defaultActiveKey={['1']}>
-                <Collapse.Panel header='最近评论' key='1'>
-                    <List
-                        dataSource={this.props.latestComments}
-                        renderItem={item => {
-                            const articleUrl = buildArticleUrl(item.to.number);
-                            return <List.Item>
-                                <List.Item.Meta
-                                    title={
-                                        <>
-                                            {item.author.name}
-                                            评论了
-                                            <Link as={articleUrl.as}
-                                                href={articleUrl.href}><a>《{item.to.title}》</a></Link>
-                                        </>
-                                    }
-                                    description={
-                                        <Typography.Text style={{ 'word-break': 'break-all' }}>
-                                            <ReactMarkdown
-                                                source={item.body}
-                                                disallowedTypes={['paragraph']}
-                                                unwrapDisallowed
-                                            />
-                                        </Typography.Text>
-                                    }
-                                />
-                            </List.Item>
-                        }}
-                    />
-                </Collapse.Panel>
-            </Collapse>
-        )
-    }
+    return (
+      <Collapse defaultActiveKey={["1"]}>
+        <Collapse.Panel header="最近评论" key="1">
+          <List
+            dataSource={this.props.latestComments}
+            renderItem={item => {
+              const articleUrl = buildArticleUrl(item.to.number);
+              return (
+                <List.Item>
+                  <List.Item.Meta
+                    title={
+                      <>
+                        <Typography.Text strong>
+                          {item.author.name}
+                        </Typography.Text>
+                        <Typography.Text type="secondary">
+                          评论了
+                        </Typography.Text>
+                        <Link as={articleUrl.as} href={articleUrl.href}>
+                          <a>
+                            <Typography.Text underline>
+                              《{item.to.title}》
+                            </Typography.Text>
+                          </a>
+                        </Link>
+                      </>
+                    }
+                    description={
+                      <Typography.Text style={{ "word-break": "break-all" }}>
+                        <ReactMarkdown
+                          source={item.body}
+                          disallowedTypes={["paragraph"]}
+                          unwrapDisallowed
+                        />
+                      </Typography.Text>
+                    }
+                  />
+                </List.Item>
+              );
+            }}
+          />
+        </Collapse.Panel>
+      </Collapse>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-    const { latestComments } = state['comment.latestComments'];
-    return { latestComments }
+  const { latestComments } = state["comment.latestComments"];
+  return { latestComments };
 }
 
 function mapDispatchToProps(dispatch) {
-    return {
-        requestLatestComments: () => dispatch(requestLatestComments())
-    }
+  return {
+    requestLatestComments: () => dispatch(requestLatestComments())
+  };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(LatestComments)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(LatestComments);
