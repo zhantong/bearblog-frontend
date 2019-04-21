96273a5f8406efacf0e145938027defa5d34d35f
Tong ZHAN <zhantong1994@163.com>
2019-04-21 14:11:24 +0800

    支持点击tag显示文章列表

--- plugins/category/article/List.js
+++ plugins/category/article/List.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Icon, Tag } from "antd";
+import Router from "next/router";
 
 class Meta extends Component {
   render() {
@@ -7,7 +8,17 @@ class Meta extends Component {
       <span>
         <Icon type="folder-open" style={{ marginRight: 8 }} />
         {this.props.data.map(category => (
-          <Tag>{category.name}</Tag>
+          <Tag
+            key={category.slug}
+            onClick={() => {
+              Router.push(
+                `/index?category=${category.slug}`,
+                `/articles?category=${category.slug}`
+              );
+            }}
+          >
+            {category.name}
+          </Tag>
         ))}
       </span>
     );
--- plugins/tag/article/Article.js
+++ plugins/tag/article/Article.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Icon, Tag } from "antd";
+import Router from "next/router";
 
 class Meta extends Component {
   render() {
@@ -7,7 +8,17 @@ class Meta extends Component {
       <span>
         <Icon type="tags" style={{ marginRight: 8 }} />
         {this.props.data.map(tag => (
-          <Tag>{tag.name}</Tag>
+          <Tag
+            key={tag.slug}
+            onClick={() => {
+              Router.push(
+                `/index?tag=${tag.slug}`,
+                `/articles?tag=${tag.slug}`
+              );
+            }}
+          >
+            {tag.name}
+          </Tag>
         ))}
       </span>
     );
