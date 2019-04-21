1868215978a4dc22778ce2e486bf0deac0d6aeef
Tong ZHAN <zhantong1994@163.com>
2019-04-21 23:21:34 +0800

    显示blog名字

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -1,56 +1,70 @@
 import React from "react";
-import {connect} from "react-redux";
-import {requestNavbar} from "../store/actions";
-import {Layout, Menu} from 'antd';
-import Link from 'next/link'
-import {buildPageUrl} from "../plugins/page/page";
-
+import { connect } from "react-redux";
+import { requestNavbar } from "../store/actions";
+import { Layout, Menu, Row, Col, Typography } from "antd";
+import Link from "next/link";
+import { buildPageUrl } from "../plugins/page/page";
+import getConfig from "next/config";
 
 class Navbar extends React.Component {
-    state = {
-        collapsed: true,
-    };
+  state = {
+    collapsed: true
+  };
 
-    onCollapse = (collapsed) => {
-        this.setState({collapsed});
-    };
+  onCollapse = collapsed => {
+    this.setState({ collapsed });
+  };
 
-    async componentDidMount() {
-        if (!this.props.pages) {
-            const {dispatch} = this.props;
-            await dispatch(requestNavbar());
-        }
+  async componentDidMount() {
+    if (!this.props.pages) {
+      const { dispatch } = this.props;
+      await dispatch(requestNavbar());
     }
+  }
 
-    render() {
-        const {pages = []} = this.props;
+  render() {
+    const { pages = [] } = this.props;
 
-        return (
-            <Layout.Header theme="light">
-                <Menu
-                    mode="horizontal"
-                    theme="dark"
-                    style={{lineHeight: '64px'}}
+    return (
+      <Layout.Header theme="light">
+        <Row>
+          <Col span={4}>
+            <Typography.Text strong style={{ fontSize: "26px" }}>
+              <Link href="/">
+                <a
+                  style={{
+                    color: "rgba(255, 255, 255, 0.65)",
+                    textDecoration: "none"
+                  }}
                 >
-                    {pages.map(page => {
-                        const pageUrl = buildPageUrl(page.slug);
-                        return (<Menu.Item>
-                            <Link as={pageUrl.as} href={pageUrl.href} passHref>
-                                <a>
-                                    {page.title}
-                                </a>
-                            </Link>
-                        </Menu.Item>)
-                    })}
-                </Menu>
-            </Layout.Header>
-        )
-    }
+                  {getConfig().publicRuntimeConfig.name}
+                </a>
+              </Link>
+            </Typography.Text>
+          </Col>
+          <Col span={20}>
+            <Menu mode="horizontal" theme="dark" style={{ lineHeight: "64px" }}>
+              {pages.map(page => {
+                const pageUrl = buildPageUrl(page.slug);
+                return (
+                  <Menu.Item>
+                    <Link as={pageUrl.as} href={pageUrl.href} passHref>
+                      <a>{page.title}</a>
+                    </Link>
+                  </Menu.Item>
+                );
+              })}
+            </Menu>
+          </Col>
+        </Row>
+      </Layout.Header>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-    const {navbarLoading, pages} = state.reducer;
-    return {navbarLoading, pages}
+  const { navbarLoading, pages } = state.reducer;
+  return { navbarLoading, pages };
 }
 
-export default connect(mapStateToProps)(Navbar)
+export default connect(mapStateToProps)(Navbar);
