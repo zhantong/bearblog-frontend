2f07b59ed6d399e8fb4fd241cdc323344115d4c7
Tong ZHAN <zhantong1994@163.com>
2019-05-27 21:17:57 +0800

    替换fastify为KOA

--- server.js
+++ server.js
@@ -1,65 +1,80 @@
-const fastify = require('fastify');
-const Next = require('next');
-const fs=require('fs')
+const Koa = require("koa");
+const Router = require("koa-router");
+const cors = require("@koa/cors");
+var bodyParser = require("koa-bodyparser");
+const Next = require("next");
+const fs = require("fs");
 
 const port = parseInt(process.env.PORT, 10) || 3000;
-const dev = process.env.NODE_ENV !== 'production';
-const app = Next({dev});
+const dev = process.env.NODE_ENV !== "production";
+const app = Next({ dev });
 const handle = app.getRequestHandler();
 
-app.prepare()
-    .then(() => {
-        const server = fastify();
-        server.register(require('fastify-cors'))
+app.prepare().then(() => {
+  const server = new Koa();
+  server.use(cors());
+  server.use(bodyParser());
+  const router = new Router();
 
-        server.get('/a/:number', (req, res) => {
-            const queryParams = {
-                _type: 'article',
-                number: req.params.number
-            };
+  router.get("/a/:number", async ctx => {
+    const queryParams = {
+      _type: "article",
+      number: ctx.params.number
+    };
 
-            return app.render(req.req, res.res, '/index', queryParams)
-        });
+    await app.render(ctx.req, ctx.res, "/index", queryParams);
+    ctx.respond = false;
+  });
 
-        server.get('/p/:slug', (req, res) => {
-            const queryParams = {
-                _type: 'page',
-                slug: req.params.slug
-            };
+  router.get("/p/:slug", async ctx => {
+    const queryParams = {
+      _type: "page",
+      slug: ctx.params.slug
+    };
 
-            return app.render(req.req, res.res, '/index', queryParams)
-        });
+    await app.render(ctx.req, ctx.res, "/index", queryParams);
+    ctx.respond = false;
+  });
 
-        server.get('/articles',(req,res)=>{
-            return app.render(req.req, res.res, '/index', req.query)
-        })
+  router.get("/articles", async ctx => {
+    await app.render(ctx.req, ctx.res, "/index", ctx.query);
+    ctx.respond = false;
+  });
 
-        server.get('/*', (req, res) => {
-            return handle(req.req, res.res)
-        });
-        server.get('/config',(req,res)=>{
-            const config=JSON.parse(fs.readFileSync('./config.json'))
-            if(req.headers.authorization!==config.SECRET_KEY){
-                res.code(401).send()
-            }else{
-                res.send(config)
-            }
-        })
-        server.post('/config',(req,res)=>{
-            const config=JSON.parse(fs.readFileSync('./config.json'))
-            if(req.headers.authorization!==config.SECRET_KEY){
-                res.code(401).send()
-            }else{
-                fs.writeFileSync('./config.json', JSON.stringify(req.body,null,2))
-                res.send({message:'OK'})
-            }
-        })
-        server.get('/attachments/*',(req,res)=>{
-            res.redirect(`https://127.0.0.1:5000/attachments/${req.params['*']}`)
-        })
-
-        server.listen(port, (err) => {
-            if (err) throw err;
-            console.log(`> Ready on http://localhost:${port}`)
-        })
-    });
+  router.get("/config", async ctx => {
+    const config = JSON.parse(fs.readFileSync("./config.json"));
+    if (ctx.request.headers.Authorization !== config.SECRET_KEY) {
+      ctx.status = 401;
+    } else {
+      ctx.body = config;
+    }
+  });
+  router.post("/config", async ctx => {
+    const config = JSON.parse(fs.readFileSync("./config.json"));
+    if (ctx.request.headers.Authorization !== config.SECRET_KEY) {
+      ctx.status = 401;
+    } else {
+      fs.writeFileSync(
+        "./config.json",
+        JSON.stringify(ctx.request.body, null, 2)
+      );
+      ctx.body = { message: "OK" };
+    }
+  });
+  router.get("/attachments/:filename", async ctx => {
+    ctx.redirect(`https://127.0.0.1:5000/attachments/${ctx.params.filename}`);
+  });
+  router.get("/*", async ctx => {
+    await handle(ctx.req, ctx.res);
+    ctx.respond = false;
+  });
+  server.use(async (ctx, next) => {
+    ctx.res.statusCode = 200;
+    await next();
+  });
+  server.use(router.routes());
+  server.listen(port, err => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
